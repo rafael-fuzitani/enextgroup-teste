ae711d9aa8ff4a0b06b1b7685fae88fa0233a5c7
Rafael Fuzitani <tofumakoto@gmail.com>
2016-10-17 19:11:35 -0200

    ingredients

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,12 @@ function ajax_get_json(){
 				var potionsObjs = data.potions[obj];
 					console.log('before nested loop!');
 					console.log(potionsObjs.ingredients);
-				for(var j = 0; j < potionsObjs.length; j++){ 
-					var objIngredients = potionsObjs.ingredients[j]; 
-						console.log(objIngredients);
-						console.log('nested loop!');
-					 }
-				potions.innerHTML += '<div class="potions-thumb"><img src="img/thumbs/'+ potionsObjs.image+'">' + '<p>' + potionsObjs.name + ' - <strong class="price">$'+ potionsObjs.price+'</strong></p></div>' + '<div class="potions-modal"><div class="potions-photo"><img src="img/'+ potionsObjs.image +'"></div><div class="potions-infos"><h2>'+ potionsObjs.name +'</h2><h2>Use/Effect</h2><p>'+ potionsObjs.effect +'</p><h2>Ingredients:</h2><ul>' + 
+				// for(var j = 0; j < potionsObjs.length; j++){ 
+				// 	var objIngredients = potionsObjs.ingredients[j]; 
+				// 		console.log(objIngredients);
+				// 		console.log('nested loop!');
+				// 	 }
+				potions.innerHTML += '<div class="potions-thumb"><img src="img/thumbs/'+ potionsObjs.image+'">' + '<p>' + potionsObjs.name + ' - <strong class="price">$'+ potionsObjs.price+'</strong></p></div>' + '<div class="potions-modal"><div class="potions-photo"><img src="img/'+ potionsObjs.image +'"></div><div class="potions-infos"><h2>'+ potionsObjs.name +'</h2><h2>Use/Effect</h2><p>'+ potionsObjs.effect +'</p><h2>Ingredients:</h2><ul>' + potionsObjs.ingredients +
 					//ingredients loop
 					
 						'</ul><h2>Price:</h2><h2 class="price">$ '+ potionsObjs.price +'</h2></div></div>';

d1c71d48c3ab72745f28e7eeb6cb467de84f3c18
Rafael Fuzitani <tofumakoto@gmail.com>
2016-10-17 16:38:52 -0200

    bunch of more JSON

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ function ajax_get_json(){
 		    var data = JSON.parse(hr.responseText);
 			potions.innerHTML = "";
 			for(var obj in data.potions){
-				potions.innerHTML += '<div class="potions-thumb"><img src="img/thumbs/'+data.potions[obj].image+'"></div>' + '<h2>' +data.potions[obj].name + '</h2><h2>Use/Effect</h2><p>' + data.potions[obj].effect + '<p><h2>Ingredients:</h2>';
+				potions.innerHTML += '<div class="potions-thumb"><img src="img/thumbs/'+data.potions[obj].image+'"></div>' + '<p>' +data.potions[obj].name + ' - <strong class="price">'+ data.potions[obj].price+'</strong></p>';
 			}
 	    }
     }
